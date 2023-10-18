76d8b07a4e6f174dc37941c28ce6e25436a325d6
Marcelo Rovai <Mjrovai@gmail.com>
2023-10-18 17:34:14 -0300

    Add files via upload

---
+++ KWS/nicla_vision_microphone_continuous_LED/nicla_vision_microphone_continuous_LED.ino
@@ -0,0 +1,318 @@
+/* Edge Impulse ingestion SDK
+ * Copyright (c) 2022 EdgeImpulse Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// If your target is limited in memory remove this macro to save 10K RAM
+#define EIDSP_QUANTIZE_FILTERBANK   0
+
+/*
+ ** NOTE: If you run into TFLite arena allocation issue.
+ **
+ ** This may be due to may dynamic memory fragmentation.
+ ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
+ ** if it doesn't exist) and copy this file to
+ ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
+ **
+ ** See
+ ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
+ ** to find where Arduino installs cores on your machine.
+ **
+ ** If the problem persists then there's not enough memory for this model and application.
+ */
+
+/* Includes ---------------------------------------------------------------- */
+#include <Nicla-Vision-KWS_inferencing.h>
+#include <PDM.h>
+
+/** Audio buffers, pointers and selectors */
+typedef struct {
+    signed short *buffers[2];
+    unsigned char buf_select;
+    unsigned char buf_ready;
+    unsigned int buf_count;
+    unsigned int n_samples;
+} inference_t;
+
+static inference_t inference;
+static volatile bool record_ready = false;
+// static signed short *sampleBuffer;
+static signed short sampleBuffer[2048];
+static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+static int print_results = -(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW);
+
+
+
+/**
+ * @brief      Arduino setup function
+ */
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    // comment out the below line to cancel the wait for USB connection (needed for native USB)
+    while (!Serial);
+    Serial.println("Inferencing - Nicla Vision KWS with LEDs");
+    
+    // Pins for the built-in RGB LEDs on the Arduino Nano 33 BLE Sense
+    pinMode(LEDR, OUTPUT);
+    pinMode(LEDG, OUTPUT);
+    pinMode(LEDB, OUTPUT);
+
+    // Ensure the LEDs are OFF by default.
+    // Note: The RGB LEDs on the Arduino Nicla Vision
+    // are ON when the pin is LOW, OFF when HIGH.
+    digitalWrite(LEDR, HIGH);
+    digitalWrite(LEDG, HIGH);
+    digitalWrite(LEDB, HIGH);
+
+    // summary of inferencing settings (from model_metadata.h)
+    ei_printf("Inferencing settings:\n");
+    ei_printf("\tInterval: ");
+    ei_printf_float((float)EI_CLASSIFIER_INTERVAL_MS);
+    ei_printf(" ms.\n");
+    ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+    ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
+    ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) /
+                                            sizeof(ei_classifier_inferencing_categories[0]));
+
+    run_classifier_init();
+    if (microphone_inference_start(EI_CLASSIFIER_SLICE_SIZE) == false) {
+        ei_printf("ERR: Could not allocate audio buffer (size %d), this could be due to the window length of your model\r\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT);
+        return;
+    }
+}
+
+/**
+ * @brief      Arduino main function. Runs the inferencing loop.
+ */
+void loop()
+{
+    bool m = microphone_inference_record();
+    if (!m) {
+        ei_printf("ERR: Failed to record audio...\n");
+        return;
+    }
+
+    signal_t signal;
+    signal.total_length = EI_CLASSIFIER_SLICE_SIZE;
+    signal.get_data = &microphone_audio_signal_get_data;
+    ei_impulse_result_t result = {0};
+
+    EI_IMPULSE_ERROR r = run_classifier_continuous(&signal, &result, debug_nn);
+    if (r != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", r);
+        return;
+    }
+
+    if (++print_results >= (EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW)) {
+        // print the predictions
+        ei_printf("Predictions ");
+        ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+            result.timing.dsp, result.timing.classification, result.timing.anomaly);
+        ei_printf(": \n");
+
+        int pred_index = 0;     // Initialize pred_index
+        float pred_value = 0;   // Initialize pred_value
+
+        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+            if (result.classification[ix].value > pred_value){
+                pred_index = ix;
+                pred_value = result.classification[ix].value;
+            }
+            // ei_printf("    %s: ", result.classification[ix].label);
+            // ei_printf_float(result.classification[ix].value);
+            // ei_printf("\n");
+        }
+        ei_printf("  PREDICTION: ==> %s with probability %.2f\n", 
+                  result.classification[pred_index].label, pred_value);
+        turn_on_leds (pred_index);
+
+        
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+        ei_printf("    anomaly score: ");
+        ei_printf_float(result.anomaly);
+        ei_printf("\n");
+#endif
+
+        print_results = 0;
+    }
+}
+
+
+/**
+ * @brief      turn_off_leds function - turn-off all RGB LEDs
+ */
+void turn_off_leds(){
+    digitalWrite(LEDR, HIGH);
+    digitalWrite(LEDG, HIGH);
+    digitalWrite(LEDB, HIGH);
+}
+
+/**
+ * @brief      turn_on_leds function used to turn on the RGB LEDs
+ * @param[in]  pred_index     
+ *             no:       [0] ==> Red ON
+ *             noise:    [1] ==> ALL OFF 
+ *             unknown:  [2] ==> Blue ON
+ *             Yes:      [3] ==> Green ON
+ */
+void turn_on_leds(int pred_index) {
+  switch (pred_index)
+  {
+    case 0:
+      turn_off_leds();
+      digitalWrite(LEDR, LOW);
+      break;
+
+    case 1:
+      turn_off_leds();
+      break;
+    
+    case 2:
+      turn_off_leds();
+      digitalWrite(LEDB, LOW);
+      break;
+
+    case 3:
+      turn_off_leds();
+      digitalWrite(LEDG, LOW);
+      break;
+  }
+}
+
+
+/**
+ * @brief      PDM buffer full callback
+ *             Copy audio data to app buffers
+ */
+static void pdm_data_ready_inference_callback(void)
+{
+    int bytesAvailable = PDM.available();
+
+    // read into the sample buffer
+    int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
+
+    if ((inference.buf_ready == 0) && (record_ready == true)) {
+        for(int i = 0; i < bytesRead>>1; i++) {
+            inference.buffers[inference.buf_select][inference.buf_count++] = sampleBuffer[i];
+
+            if (inference.buf_count >= inference.n_samples) {
+                inference.buf_select ^= 1;
+                inference.buf_count = 0;
+                inference.buf_ready = 1;
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * @brief      Init inferencing struct and setup/start PDM
+ *
+ * @param[in]  n_samples  The n samples
+ *
+ * @return     { description_of_the_return_value }
+ */
+static bool microphone_inference_start(uint32_t n_samples)
+{
+    inference.buffers[0] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+    if (inference.buffers[0] == NULL) {
+        return false;
+    }
+
+    inference.buffers[1] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+    if (inference.buffers[1] == NULL) {
+        ei_free(inference.buffers[0]);
+        return false;
+    }
+
+    inference.buf_select = 0;
+    inference.buf_count = 0;
+    inference.n_samples = n_samples;
+    inference.buf_ready = 0;
+
+    // configure the data receive callback
+    PDM.onReceive(&pdm_data_ready_inference_callback);
+
+    PDM.setBufferSize(2048);
+    delay(250);
+    
+    // initialize PDM with:
+    // - one channel (mono mode)
+    if (!PDM.begin(1, EI_CLASSIFIER_FREQUENCY)) {
+        ei_printf("ERR: Failed to start PDM!");
+        return false;
+    }
+
+    // optionally set the gain, defaults to 24
+    // Note: values >=52 not supported
+    //PDM.setGain(40);
+
+    record_ready = true;
+
+    return true;
+}
+
+/**
+ * @brief      Wait on new data
+ *
+ * @return     True when finished
+ */
+static bool microphone_inference_record(void)
+{
+    bool ret = true;
+
+    if (inference.buf_ready == 1) {
+        ei_printf(
+            "Error sample buffer overrun. Decrease the number of slices per model window "
+            "(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW)\n");
+        ret = false;
+    }
+
+    while (inference.buf_ready == 0) {
+        delay(1);
+    }
+
+    inference.buf_ready = 0;
+
+    return ret;
+}
+
+/**
+ * Get raw audio signal data
+ */
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    numpy::int16_to_float(&inference.buffers[inference.buf_select ^ 1][offset], out_ptr, length);
+
+    return 0;
+}
+
+/**
+ * @brief      Stop PDM and release buffers
+ */
+static void microphone_inference_end(void)
+{
+    PDM.end();
+    ei_free(inference.buffers[0]);
+    ei_free(inference.buffers[1]);
+    record_ready = false;
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
